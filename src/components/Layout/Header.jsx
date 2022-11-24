490686b422c379b13930103c669526b998cf0606
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-11-24 23:30:16 +0530

    added the cart button component of 133 video

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { mealsJPG } from "../../assets";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button onClick={props.onShowCart}>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsJPG} alt="meals"></img>
